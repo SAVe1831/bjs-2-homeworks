3dcd34c968c617809853dad035440e5ebe8a51a7
Александр Швецов <save1831537@gmail.com>
2023-04-07 00:27:43 +0300

    Первое задание полностью готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,22 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let arr = [];
+	let d = b ** 2 - 4 * a * c;
+	if (d == 0) {
+		arr.push(-b / (2 * a));
+	} else if (d > 0) {
+		arr.push((-b + Math.sqrt(d)) / (2 * a));
+		arr.push((-b - Math.sqrt(d)) / (2 * a));
+	}
+	return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+	let s = Number(amount) - Number(contribution);       //Тело кредита
+	let p = (Number(percent) / 100) / 12;                //Коэффициент = 1/12 месячной ставки
+	let n = Number(countMonths);                         //Количество месяцев
+	let monthPay = (s * (p + (p / (((1 + p) ** n) - 1))));    //Ежемесячный платеж
+	let allPay = +(monthPay * n).toFixed(2);                  ////Общая сумма
+	return allPay;
 }
